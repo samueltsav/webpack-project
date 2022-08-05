51d9cb3415d9ff056ae034d913ada8fb383f3b0d
Samuel Tsav <tsavsamuel@gmail.com>
2022-08-06 00:54:04 +0100

    Adds Babel to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const elvenShieldRecipe = {
     leatherStrips: 2,
-    ironIgot: 1,
+    ironIngot: 1,
     refinedMoonstone: 4,
 };
 
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4,
+}
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
